308ce12e797dd6c066fc88c2c545c0a4e30b6a9c
Felix Wiegand <fxwiegand@wgdnet.de>
2024-11-04 10:53:08 +0100

    Add missing @ for decoration

--- test_spells.py
+++ test_spells.py
@@ -7,7 +7,7 @@ import subprocess as sp
 
 spells = [Path(meta_path).parent for meta_path in glob.glob("**/meta.yaml")]
 
-pytest.mark.parametrize("spell", spells)
+@pytest.mark.parametrize("spell", spells)
 def test_spell(spell):
     tests = list((spell / "tests").glob("*"))
     assert tests, f"No tests found for {spell}"
