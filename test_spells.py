baeb9667a5cab4709e5b32b54f869addb1922042
Johannes Koester <johannes.koester@uni-due.de>
2024-11-04 11:02:55 +0100

    fixes

--- test_spells.py
+++ test_spells.py
@@ -5,14 +5,20 @@ import tempfile
 import pytest
 import subprocess as sp
 
-spells = [Path(meta_path).parent for meta_path in glob.glob("**/meta.yaml")]
+spells = [meta_path.parent for meta_path in Path(".").glob("**/meta.yaml")]
 
-pytest.mark.parametrize("spell", spells)
+@pytest.mark.parametrize("spell", spells)
 def test_spell(spell):
-    tests = list((spell / "tests").glob("*"))
+    tests = list((spell / "tests").glob("test_*.yaml"))
     assert tests, f"No tests found for {spell}"
 
     for testcase in tests:
-        config = testcase / "config.yaml"
+        test_dir = testcase.parent
+        print(f"Testcase {testcase}:", file=sys.stderr)
         with tempfile.TemporaryDirectory() as tmpdir:
-            sp.run(["datavzrd", config, "--output", tmpdir], check=True, stderr=sp.PIPE, cwd=testcase)
+            try:
+                sp.run(["datavzrd", testcase.name, "--output", tmpdir], check=True, stderr=sp.PIPE, cwd=test_dir)
+            except sp.CalledProcessError as e:
+                print(e.stderr.decode(), file=sys.stderr)
+                raise e
+            print("OK", file=sys.stderr)
