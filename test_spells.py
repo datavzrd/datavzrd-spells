e30afbdff74771ca56ab05132c008fe5f8647930
Johannes Koester <johannes.koester@uni-due.de>
2024-11-04 11:18:33 +0100

    outputs

--- test_spells.py
+++ test_spells.py
@@ -1,11 +1,17 @@
 import glob
 from pathlib import Path
+import shutil
 import sys
 import tempfile
 import pytest
 import subprocess as sp
 
 spells = [meta_path.parent for meta_path in Path(".").glob("**/meta.yaml")]
+
+outdir_base = Path("test_outputs")
+if outdir_base.exists():
+    print("Cleaning up previous test outputs", file=sys.stderr)
+    shutil.rmtree(outdir_base)
 
 @pytest.mark.parametrize("spell", spells)
 def test_spell(spell):
@@ -14,11 +20,12 @@ def test_spell(spell):
 
     for testcase in tests:
         test_dir = testcase.parent
-        print(f"Testcase {testcase}:", file=sys.stderr)
-        with tempfile.TemporaryDirectory() as tmpdir:
-            try:
-                sp.run(["datavzrd", testcase.name, "--output", tmpdir], check=True, stderr=sp.PIPE, cwd=test_dir)
-            except sp.CalledProcessError as e:
-                print(e.stderr.decode(), file=sys.stderr)
-                raise e
-            print("OK", file=sys.stderr)
+        outdir = (outdir_base / test_dir / testcase.name).with_suffix("")
+        outdir.mkdir(parents=True, exist_ok=True)
+        print(f"Testcase {testcase}, storing result it {outdir}:", file=sys.stderr)
+        try:
+            sp.run(["datavzrd", testcase.name, "--output", outdir.absolute()], check=True, stderr=sp.PIPE, cwd=test_dir)
+        except sp.CalledProcessError as e:
+            print(e.stderr.decode(), file=sys.stderr)
+            raise e
+        print("OK", file=sys.stderr)
